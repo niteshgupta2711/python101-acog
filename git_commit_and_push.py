6759d12170868f0f58a03eca7dd57933c5e82ce3
Nitesh Kumar Gupta <nitesh@SezazAir2.aganitha.ai>
2022-12-14 12:41:56 +0530

    git push automated

--- git_commit_and_push.py
+++ git_commit_and_push.py
@@ -6,7 +6,7 @@ app = typer.Typer()
 
 @app.command()
 def commit_push(mess: str = "git push automated", branch: str = "main") -> None:
-    os.system(f'git add . && git commit -m "{mess}" && git push -u origin "branch"')
+    os.system(f'git add . && git commit -m "{mess}" && git push -u origin {branch} ')
 
 
 if __name__ == "__main__":
